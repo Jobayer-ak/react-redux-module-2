facab75314efb225814d5c94c5f592b24d3763a9
Jobayer_ak <jobayer.ak@gmail.com>
2022-08-22 21:45:23 +0600

    applyMiddleware()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
+// middle ware
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}}`)
+   
+    const upcomingState = [action].reduce(rootReducer, store.getState());
 
-const store = createStore(rootReducer); // store accepts reducer as a parameter;
+    console.log(JSON.stringify(upcomingState));
+
+    // pass action
+    return next(action);
+} 
+
+const store = createStore(rootReducer, applyMiddleware(myLogger)); // store accepts reducer as a parameter;
 
 
 export default store;
